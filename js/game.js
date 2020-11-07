5f46fc6a6c875f8673d9c5e81f00eaa101cc9ac0
Alvaro A Pansa <alvaro.pansa@gmail.com>
2020-11-07 09:45:00 -0300

    Section 2 added

--- js/game.js
+++ js/game.js
@@ -1,27 +1,36 @@
-//1. Drawing in Canvas
-var canvas = null, ctx = null;
+var canvas = null, ctx = null, x = 50, y = 50;
+window.requestAnimationFrame = (function () {
+    return window.requestAnimationFrame || 
+    window.mozRequestAnimationFrame || 
+    window.webkitRequestAnimationFrame || 
+    function (callback) {
+        window.setTimeout(callback, 17);
+    };
+}());
 function paint(ctx) {
-    ctx.fillStyle = '#0f0'; // The style of the fill comand below
-    ctx.fillRect(50, 50, 100, 60);// Draws a filled rectangle (coord x, coord y, width, height)
-    /*ctx.strokeStyle = '#00f';
-    ctx.strokeRect(40, 40, 120, 80); // Draws an empty rectangle
-    ctx.fillStyle = '#ff0';
-    ctx.fillRect(0, 0, 20, 150);
-    ctx.fillRect(40, 0, 20, 150);
-    ctx.fillRect(80, 0, 20, 150);
-    ctx.fillRect(120, 0, 20, 150);
-    ctx.fillRect(160, 0, 20, 150);
-    ctx.fillStyle = '#00f';
-    ctx.fillRect(20, 0, 20, 150);
-    ctx.fillRect(60, 0, 20, 150);
-    ctx.fillRect(100, 0, 20, 150);
-    ctx.fillRect(140, 0, 20, 150);
-    ctx.fillRect(180, 0, 20, 150);*/
+    //ctx.fillStyle = '#0f0'; // The style of the fill comand below
+    //ctx.fillRect(50, 50, 100, 60);// Draws a filled rectangle (coord x, coord y, width, height)
+    //ctx.strokeStyle = '#00f';
+    //ctx.strokeRect(40, 40, 120, 80); // Draws an empty rectangle
+    ctx.fillStyle = '#000';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = '#0f0';
+    ctx.fillRect(x, y, 10, 10);
+}
+function act(){
+    x += 2;
+    if (x > canvas.width) {
+        x = 0;
+    }
+}
+function run() {
+    window.requestAnimationFrame(run);
+    act();
+    paint(ctx);
 }
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-    paint(ctx);
+    run();
 }
 window.addEventListener('load', init, false);
-//2. Animating the canvas
