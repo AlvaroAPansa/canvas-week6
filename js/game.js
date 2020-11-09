b22496028ccbd40cd6d715d043d28be94b26030d
Alvaro A Pansa <alvaro.pansa@gmail.com>
2020-11-09 00:34:46 -0300

    Unused sections deleted

--- js/game.js
+++ js/game.js
@@ -182,142 +182,6 @@
             return false;
         }
     }
-    
-    /*function reset() {
-        score = 0;
-        dir = 1;
-        body.length = 0;
-        body.push(new Rectangle(40, 40, 10, 10));
-        body.push(new Rectangle(0, 0, 10, 10));
-        body.push(new Rectangle(0, 0, 10, 10));
-        food.x = random(buffer.width / 10 - 1) * 10;
-        food.y = random(buffer.height / 10 - 1) * 10;
-        gameover = false;
-    }*/
-
-    /*function paint(ctx) {
-        var i = 0,
-            l = 0;
-        // Clean canvas
-        ctx.fillStyle = '#030';
-        ctx.fillRect(0, 0, buffer.width, buffer.height);
-        // Draw player
-        ctx.strokeStyle = '#0f0';
-        for (i = 0, l = body.length; i < l; i++) {
-            body[i].drawImage(ctx, iBody);
-        }
-        // Draw walls
-        /*ctx.fillStyle = '#999';
-        for(i = 0 ,l = wall.length; i < l; i += 1) {
-            wall[i].fill(ctx);
-        }*/
-        /*// Draw food
-        ctx.strokeStyle = '#f00';
-        food.drawImage(ctx, iFood);
-        // Debug last key pressed
-        //ctx.fillText('Last Press: ' + lastPress, 0, 20);
-        // Draw score
-        ctx.fillStyle = '#fff';
-        ctx.fillText('Score: ' + score, 0, 10);
-        // Draw pause
-        if (pause) {
-            ctx.textAlign = 'center';
-            if (gameover) {
-                ctx.fillText('GAME OVER', 150, 75);
-            } else {
-                ctx.fillText('PAUSE', 150, 75);
-            }
-            ctx.textAlign = 'left';
-        }
-        ctx.fillText('FPS: ' + FPS, 10, 20);
-    }*/
-
-    /*function act() {
-        var i = 0,
-            l = 0;
-        if (!pause) {
-            // GameOver Reset
-            if (gameover) {
-                reset();
-            }
-            // Move Body
-            for (i = body.length - 1; i > 0; i -= 1) {
-                body[i].x = body[i - 1].x;
-                body[i].y = body[i - 1].y;
-            }
-            // Change Direction
-            if (lastPress === KEY_UP && dir !== 2) {
-                dir = 0;
-            }
-            if (lastPress === KEY_RIGHT && dir !== 3) {
-                dir = 1;
-            }
-            if (lastPress === KEY_DOWN && dir !== 0) {
-                dir = 2;
-            }
-            if (lastPress === KEY_LEFT && dir !== 1) {
-                dir = 3;
-            }
-            // Move Head
-            if (dir === 0) {
-                body[0].y -= 10;
-            }
-            if (dir === 1) {
-                body[0].x += 10;
-            }
-            if (dir === 2) {
-                body[0].y += 10;
-            }
-            if (dir === 3) {
-                body[0].x -= 10;
-            }
-            // Out Screen
-            if (body[0].x > buffer.width - body[0].width) {
-                body[0].x = 0;
-            }
-            if (body[0].y > buffer.height - body[0].height) {
-                body[0].y = 0;
-            }
-            if (body[0].x < 0) {
-                body[0].x = buffer.width - body[0].width;
-            }
-            if (body[0].y < 0) {
-                body[0].y = buffer.height - body[0].height;
-            }
-            // Wall Intersects
-            /*for(i = 0, l = wall.length; i < l; i += 1){
-                if (food.intersects(wall[i])) {
-                    food.x = random(buffer.width / 10 - 1) * 10;
-                    food.y = random(buffer.height / 10 - 1) * 10;
-                }
-                if(body[0].intersects(wall[i])){
-                    gameover = true;
-                    pause = true;
-                }
-            }*/
-            /*// Body Intersects
-            for (i = 2, l = body.length; i < l; i++) {
-                if (body[0].intersects(body[i])) {
-                    gameover = true;
-                    pause = true;
-                    aDie.play();
-                }
-            }
-            // Food Intersects
-            if (body[0].intersects(food)) {
-                body.push(new Rectangle(0, 0, 10, 10));
-                score += 1;
-                food.x = random(buffer.width / 10 - 1) * 10;
-                food.y = random(buffer.height / 10 - 1) * 10;
-                aEat.play();
-            }
-        }
-        // Pause/Unpause
-        if (lastPress === KEY_ENTER) {
-            pause = !pause;
-            lastPress = undefined;
-        }
-    }*/
 
     function repaint() {
         window.requestAnimationFrame(repaint);
@@ -443,6 +307,11 @@
         for (i = 0, l = body.length; i < l; i++) {
             body[i].drawImage(ctx, iBody);
         }
+        // Draw walls
+        /*ctx.fillStyle = '#999';
+        for(i = 0 ,l = wall.length; i < l; i += 1) {
+            wall[i].fill(ctx);
+        }*/
         // Draw food
         ctx.strokeStyle = '#f00';
         food.drawImage(ctx, iFood);
@@ -522,6 +391,17 @@
             if (body[0].y < 0) {
                 body[0].y = buffer.height - body[0].height;
             }
+            // Wall Intersects
+            /*for(i = 0, l = wall.length; i < l; i += 1){
+                if (food.intersects(wall[i])) {
+                    food.x = random(buffer.width / 10 - 1) * 10;
+                    food.y = random(buffer.height / 10 - 1) * 10;
+                }
+                if(body[0].intersects(wall[i])){
+                    gameover = true;
+                    pause = true;
+                }
+            }*/
             // Body Intersects
             for (i = 2, l = body.length; i < l; i++) {
                 if (body[0].intersects(body[i])) {
@@ -552,7 +432,7 @@
                 fruitCount = 0;// Resets the count that will trigger the bonus
                 countTrigger = defineTrigger();// Defines a new point to trigger the bonus
                 bonusTrigger = false;//Eliminates sections that paint and intersects the bonus
-                if (firstRequest) {
+                if (firstRequest) {// Adds the query params to the URL
                     scoreUrl += '?score=' + score;
                     firstRequest = false;
                     console.log(scoreUrl);
